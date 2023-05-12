4a2188064b7aa13af47707a47050b648a7d08249
Basilio9210 <basilio9210@gmail.com>
2023-05-12 14:30:17 -0500

    DEACTIVATE VIDEOS

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,9 +3,15 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
    chromeWebSecurity: false,
+   video: false,
+   screenshotOnRunFailure: false,
    setupNodeEvents(on, config) {
+    config.defaultCommandTimeout= 10000;
+    config.responseTimeout= 20000;
+    config.pageLoadTimeout= 60000;
      // e2e testing node events setup code
      return config;
     },
   },
 });
+
