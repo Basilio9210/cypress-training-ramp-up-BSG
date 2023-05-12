08eaa299e15a37c93ae78cf4dab86e701f863166
Basilio9210 <basilio9210@gmail.com>
2023-05-12 15:17:29 -0500

    Tshirt testing

--- cypress/e2e/buy-shirt.cy.ts
+++ cypress/e2e/buy-shirt.cy.ts
@@ -5,7 +5,18 @@ describe("Buy a black t-shirt", () => {
         cy.get(".login-box > form > div > input#password").type("secret_sauce"); //(2)
         cy.get("input[type='submit']").click(); //(2)
 
+
         // Debes completar la prueba con los puntos 3 al 11 del flujo
+        cy.get("[data-test='add-to-cart-sauce-labs-backpack']").click();//(3)
+        cy.get(".shopping_cart_link").click();//4
+        cy.get("[data-test='checkout']").click();//5
+        cy.get("[data-test='firstName']").type("Cypress");//(6)
+        cy.get("[data-test='lastName']").type("Workshop");//(7)
+        cy.get("[data-test='postalCode']").type("00000");//(8)
+        cy.get("[data-test='continue']").click();//(9)
+        cy.get("[data-test='finish']").click();//(10)
+
+
 
         cy.get("#contents_wrapper > .checkout_complete_container > h2").should(
             "have.text",
